2bfeca0697cdc3fa34d3545a44f86bf9f9543253
JSM <JSM@>
2025-09-02 18:56:13 +0900

    feat/폼 검증 및 개선

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,8 +1,6 @@
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex min-h-screen justify-center pt-[110px]">
-      {children}
-    </div>
+    <div className="flex min-h-screen justify-center pt-[80px]">{children}</div>
   );
 };
 
