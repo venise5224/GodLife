a540dd1f7fa6db31e8e73982d72cd5fa2814783d
JSM <JSM@>
2025-09-04 02:07:02 +0900

    refactor/활동 목록 반응형 개선

--- src/components/ActivityList/RowItem.tsx
+++ src/components/ActivityList/RowItem.tsx
@@ -8,12 +8,12 @@ interface Props {
 
 const RowItem = ({ row }: Props) => {
   return (
-    <li className="flex items-center gap-2 p-2 border rounded-lg">
+    <li className="flex items-center gap-9 sm:gap-2 p-2 border rounded-lg">
       {/* 이름 */}
-      <div className="flex-1">{row.name}</div>
+      <div className="flex-1 text-xs sm:text-base">{row.name}</div>
 
       {/* Source */}
-      <div className="w-13 text-center">
+      <div className="hidden sm:block w-13 text-center">
         <span
           className={`px-2 py-0.5 rounded-full text-xs border
           ${
@@ -29,11 +29,18 @@ const RowItem = ({ row }: Props) => {
       </div>
 
       {/* 시작 */}
-      <div className="w-30 text-center text-sm">{row.start ?? "-"}</div>
+      <div className="hidden sm:block sm:w-30 text-center text-xs sm:text-sm">
+        {row.start ?? "-"}
+      </div>
 
       {/* 종료 */}
-      <div className="w-30 text-center text-sm">
-        {row.end ??
+      <div className="hidden sm:block sm:w-30 text-center text-xs sm:text-sm">
+        {row.end ?? "-"}
+      </div>
+
+      {/* 걸린 시간 */}
+      <div className="w-14 sm:w-25 text-center text-xs sm:text-sm">
+        {row.duration ??
           (row.kind === "activity" ? (
             <span className="text-red-500">{"진행중"}</span>
           ) : (
@@ -41,12 +48,9 @@ const RowItem = ({ row }: Props) => {
           ))}
       </div>
 
-      {/* 걸린 시간 */}
-      <div className="w-25 text-center text-sm">{row.duration ?? "-"}</div>
-
       {/* 삭제 */}
-      <div className="w-16 text-center text-sm">
-        {!(row.kind === "activity" && !row.end) && (
+      <div className="w-5 sm:w-16 text-center text-sm">
+        {!(row.kind === "activity" && !row.end) ? (
           <button
             onClick={row.onDelete}
             className="text-red-500 hover:text-red-700"
@@ -55,6 +59,8 @@ const RowItem = ({ row }: Props) => {
           >
             🗑
           </button>
+        ) : (
+          <div>-</div>
         )}
       </div>
     </li>
